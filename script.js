e2c098e003a72049722f8200e26ec535faeb42e2
norakoiv-tlu <norakoiv@tlu.ee>
2026-03-30 18:34:23 +0300

    ajatelje muutused

--- script.js
+++ script.js
@@ -36,3 +36,29 @@ collapseButton.addEventListener('click', () => {
     timelineContainer.style.display = 'none';
   }, 600);
 });
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeModal = document.getElementById("modalClose");
+
+// Select ALL timeline images
+const images = document.querySelectorAll(".event-image img");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
+
+// Close modal
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Also close when clicking outside image
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
